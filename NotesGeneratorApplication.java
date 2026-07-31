619094136c6d2d152e731574478ae50844d135b6
praveenraja23 <2kdaydreamer23@gmail.com>
2026-07-31 08:17:35 +0530

    Add files via upload

---
+++ NotesGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.notesgen.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesGeneratorApplication.class, args);
+    }
+}
